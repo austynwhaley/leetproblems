f3cd98ee76801cbe7ff8899481650fb288fddf5f
austynw <austyn_whaley@yahoo.com>
2024-11-06 16:29:40 -0500

    4:29pm

--- probz/227.py
+++ probz/227.py
@@ -1,5 +1,24 @@
 class Solution:
     def calculate(self, s: str) -> int:
+        # create a stack
+        stack = []
+        # create placeholder num and oper
+        current_num = 0
+        current_oper = "+"
+        # parse the string seperate nums and and operaters
+        for i, char in str:
+            if char.isdigit():
+                current_number = current_number * 10 + int(char)
+
+            if char in "+-*/" or len(s) - 1:
+                if current_oper
+
 
         
-        return
+        return s
+    
+
+solution = Solution()
+expression = "3+2*2"
+
+print(solution.calculate(expression))
