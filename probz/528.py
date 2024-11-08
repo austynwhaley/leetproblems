cbcad9e4549c32cc73f2e5460a963bdeb6626a94
austynw <austyn_whaley@yahoo.com>
2024-11-08 16:41:40 -0500

    new solved

--- probz/528.py
+++ probz/528.py
@@ -1,9 +1,27 @@
+import random
+import bisect
+from typing import List
+
+
 class Solution:
 
     def __init__(self, w: List[int]):
+        self.w = []
+        total = 0
+        for weight in w:
+            total += weight
+            self.w.append(total)
+        self.total = total
+
         
 
     def pickIndex(self) -> int:
+        # this func need to pick an index randomly
+        # that is within the range with higher nums
+        # increasing the probablity
+        r_num = random.randint(1, self.total)
+        bisect.bisect_left(self.w, r_num)
+        
         
 
 
